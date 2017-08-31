6ed8c8c22d75a0447a5e147193727803fb3f6210
loriteng <loriteng@gmail.com>
2017-08-31 11:45:49 +0800

    Initial commit

---
+++ app/src/main/java/es/esy/mobilehost/android/tempgallery/CreateList.java
@@ -0,0 +1,31 @@
+package es.esy.mobilehost.android.tempgallery;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2017/8/31.
+ */
+public class CreateList {
+
+    private String image_title;
+    private Integer image_id;
+
+    public String getImage_title() {
+        return image_title;
+    }
+
+    public void setImage_title(String android_version_name) {
+        this.image_title = android_version_name;
+    }
+
+    public Integer getImage_ID() {
+        return image_id;
+    }
+
+    public void setImage_ID(Integer android_image_url) {
+        this.image_id = android_image_url;
+    }
+
+
+}
+
